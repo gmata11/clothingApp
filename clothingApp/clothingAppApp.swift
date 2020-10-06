5e2a913e121736f831955b5dd303c17737cf4040
gmata11 <gerard@dekalabs.com>
2020-10-06 14:46:31 +0200

    First commit

---
+++ clothingApp/clothingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  clothingAppApp.swift
+//  clothingApp
+//
+//  Created by Gerard Mata Carrera on 06/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct clothingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
